b55edf76d3090cc0a0ee521e2e55c94dd0b9d492
Walter vanegas <walter.vanegas@globant.com>
2022-04-27 18:52:42 -0500

    clase 20

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 
+const datos = require('./public/productsDataBase.json');
+
 //Routes imports
 const personas = require('./routes/personas');
 const productos = require('./routes/productos');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'));
 app.use('/personas', personas);
 app.use('/productos', productos);
 
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log('Servidor corriendo en el puerto 3000');
+app.get('/', (req, res) => {
+    //codigo que llama funciones del controlador
+    
+    res.render('index', { 
+        datos,        
+     });
+});
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
